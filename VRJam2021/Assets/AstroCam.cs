42fec743d94a78c750bd2b04b14d98de5bcb9cf7
blobworks <83734745+blobworks@users.noreply.github.com>
2021-07-31 17:11:00 +0100

    build test

---
+++ VRJam2021/Assets/AstroCam.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroCam : MonoBehaviour
+{
+
+    GameManager gameManager; 
+    [SerializeField] public Transform camSphere; 
+    [SerializeField] public Transform povCam; 
+    [SerializeField] Camera camera; 
+    [SerializeField] Rigidbody rb;
+
+    bool camSphereToggled; 
+
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>(); 
+    }
+
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryThumbstick, OVRInput.Controller.RTouch))
+        {
+            if(!camSphereToggled)
+            {
+                camera.transform.position = camSphere.position;
+                camera.transform.rotation = camSphere.rotation;
+                camera.transform.parent = camSphere;
+                camSphereToggled = true; 
+            }
+            else
+            {
+                camera.transform.position = povCam.position; 
+                camera.transform.rotation = povCam.rotation; 
+                camera.transform.parent = povCam; 
+                camSphereToggled = false; 
+            }
+        }
+        
+        if(camSphereToggled)
+        {
+            transform.position = rb.transform.position; 
+        }
+    }
+}
